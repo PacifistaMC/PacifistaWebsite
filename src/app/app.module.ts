aefb2f18bbf8634b57dd88723584211744374cbf
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-03-29 16:25:26 +0200

    SEO upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, Title} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +38,9 @@ import {HttpClientModule} from "@angular/common/http";
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/legal/cgv/cgv.component.ts
+++ src/app/pages/legal/cgv/cgv.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-cgv',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./cgv.component.scss']
 })
 export class CgvComponent {
+
+  constructor(private titleService: Title) {
+    const title: string = titleService.getTitle();
+
+    if (!title.startsWith("CGV")) {
+      titleService.setTitle('CGV - ' + title);
+    }  }
 
 }
