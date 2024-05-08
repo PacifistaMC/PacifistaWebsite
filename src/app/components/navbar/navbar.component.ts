ea2f60d77c16b6ba36514d273704e0a0e47fa0f3
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2024-05-08 23:40:54 +0200

    done current nav (#55)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {PlatformLocation} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +7,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  currentUrl: string;
+
+  constructor(platformLocation: PlatformLocation) {
+    this.currentUrl = platformLocation.pathname;
+  }
+
 }
