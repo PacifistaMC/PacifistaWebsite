a30a74c366050eb1926b17b8f68b5ecf4cd43646
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-05-10 19:59:33 +0200

    wip . billing

--- src/app/pages/user/user-page/components/user-account-invoices/user-account-invoices.component.ts
+++ src/app/pages/user/user-page/components/user-account-invoices/user-account-invoices.component.ts
@@ -1,13 +1,71 @@
-import {Component, Input} from '@angular/core';
-import {UserDTO} from "@funixproductions/funixproductions-requests";
+import {AfterViewInit, Component, Input} from '@angular/core';
+import {
+  FunixprodBillingDto,
+  FunixprodBillingService,
+  PageOption,
+  QueryBuilder,
+  UserDTO
+} from "@funixproductions/funixproductions-requests";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../../../environments/environment";
+import NotificationService from "../../../../../services/notifications/services/NotificationService";
 
 @Component({
   selector: 'app-user-account-invoices',
   templateUrl: './user-account-invoices.component.html',
   styleUrl: './user-account-invoices.component.scss'
 })
-export class UserAccountInvoicesComponent {
+export class UserAccountInvoicesComponent implements AfterViewInit {
 
+  private readonly billingService: FunixprodBillingService
   @Input() user: UserDTO = new UserDTO()
+
+  page: number = 0
+  invoices: FunixprodBillingDto[] = []
+  loading: boolean = true
+
+  constructor(httpClient: HttpClient, private notificationService: NotificationService) {
+    this.billingService = new FunixprodBillingService(httpClient, environment.production)
+  }
+
+  ngAfterViewInit(): void {
+    this.refreshList()
+  }
+
+  pageUp() {
+    ++this.page
+    this.refreshList()
+  }
+
+  pageDown() {
+    if (this.page > 0) {
+      --this.page
+      this.refreshList()
+    }
+  }
+
+  download(billing: FunixprodBillingDto) {
+    if (!billing.id) return
+    this.billingService.downloadInvoice(billing.id)
+  }
+
+  private refreshList() {
+    const pageOption = new PageOption()
+    pageOption.sort = "createdAt:desc"
+    pageOption.elemsPerPage = 10
+    pageOption.page = this.page
+
+    this.loading = true
+    this.billingService.find(pageOption, new QueryBuilder()).subscribe({
+      next: page => {
+        this.invoices = page.content
+        this.loading = false
+      },
+      error: err => {
+        this.notificationService.onErrorRequest(err)
+        this.loading = false
+      }
+    })
+  }
 
 }
