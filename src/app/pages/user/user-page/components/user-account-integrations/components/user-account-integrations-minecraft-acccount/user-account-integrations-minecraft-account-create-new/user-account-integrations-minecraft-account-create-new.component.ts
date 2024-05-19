390aeea19e634d293f391f5e58b0a8d6ced71178
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-05-19 14:46:20 +0200

    wip minecraft link

--- src/app/pages/user/user-page/components/user-account-integrations/components/user-account-integrations-minecraft-acccount/user-account-integrations-minecraft-account-create-new/user-account-integrations-minecraft-account-create-new.component.ts
+++ src/app/pages/user/user-page/components/user-account-integrations/components/user-account-integrations-minecraft-acccount/user-account-integrations-minecraft-account-create-new/user-account-integrations-minecraft-account-create-new.component.ts
@@ -1,14 +1,26 @@
 import {Component, Input} from '@angular/core';
-import {ErrorDto, PacifistaWebUserLinkService, UserDTO} from "@funixproductions/funixproductions-requests";
+import {
+  ErrorDto,
+  PacifistaWebUserLinkDTO,
+  PacifistaWebUserLinkService,
+  UserDTO
+} from "@funixproductions/funixproductions-requests";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {HttpClient} from "@angular/common/http";
 import NotificationService from "../../../../../../../../services/notifications/services/NotificationService";
+import {InputTextComponent} from "../../../../../../../../components/inputs/input-text/input-text.component";
+import {SendButtonComponent} from "../../../../../../../../components/buttons/send-button/send-button.component";
+import {NgIf} from "@angular/common";
 import {environment} from "../../../../../../../../../environments/environment";
 
 @Component({
   selector: 'app-user-account-integrations-minecraft-account-create-new',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextComponent,
+    SendButtonComponent,
+    NgIf
+  ],
   templateUrl: './user-account-integrations-minecraft-account-create-new.component.html',
   styleUrl: './user-account-integrations-minecraft-account-create-new.component.scss'
 })
@@ -17,30 +29,54 @@ export class UserAccountIntegrationsMinecraftAccountCreateNewComponent {
   private readonly linkService: PacifistaWebUserLinkService
 
   @Input() user?: UserDTO
+
+  link?: PacifistaWebUserLinkDTO
+
   minecraftUsername: string = ''
   errorStr?: string
+  loading: boolean = false
 
   constructor(public activeModal: NgbActiveModal,
               private notificationService: NotificationService,
               http: HttpClient) {
     this.linkService = new PacifistaWebUserLinkService(http, environment.production)
+
+    this.linkService.getCurrentUserLink().subscribe({
+      next: (link) => {
+        if (link.linked === true) {
+          this.close()
+        }
+        this.link = link
+      },
+      error: (error: ErrorDto) => {
+        this.link = undefined
+
+        if (error.status !== 404) {
+          this.notificationService.onErrorRequest(error)
+        }
+      }
+    })
   }
 
   createLink(): void {
     this.errorStr = undefined
+    this.link = undefined
 
     if (this.minecraftUsername.length === 0) {
       this.errorStr = 'Le nom d\'utilisateur Minecraft ne peut pas être vide.';
       return
     }
 
+    this.loading = true
     this.linkService.linkUser(this.minecraftUsername).subscribe({
       next: (link) => {
-        this.activeModal.close()
+        this.loading = false
+        this.link = link
         this.notificationService.info('Le compte Minecraft a été lié avec succès.')
       },
       error: (error: ErrorDto) => {
         this.errorStr = error.error
+        this.loading = false
       }
     })
   }
