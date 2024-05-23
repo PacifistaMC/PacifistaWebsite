310d15b8cff84e4ea62120d337ade7fdd88793b7
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2024-05-23 18:26:24 +0200

    Feature/staff members add (#57)
    * wip staff members
    
    * done staff list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,46 +13,52 @@ import {NotificationComponent} from "./components/notification/notification.comp
 import {AccueilComponent} from "./pages/accueil/accueil.component";
 import {WelcomeAccueilSectionComponent} from "./pages/accueil/components/welcome/welcome-accueil-section.component";
 import {
-  AboutDetailsAccueilSectionComponent
+    AboutDetailsAccueilSectionComponent
 } from "./pages/accueil/components/about-details/about-details-accueil-section.component";
 import {NewsAccueilSectionComponent} from "./pages/accueil/components/news/news-accueil-section.component";
 import {SocialsAccueilSectionComponent} from "./pages/accueil/components/socials/socials-accueil-section.component";
 import {StaffAccueilSectionComponent} from "./pages/accueil/components/staff/staff-accueil-section.component";
 import {AboutAccueilSectionComponent} from "./pages/accueil/components/about/about-accueil-section.component";
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {MinecraftHeadComponent} from "./components/minecraft-head/minecraft-head.component";
+import {
+    StaffAccueuilSectionRowPlayerComponent
+} from "./pages/accueil/components/staff/staff-accueuil-section-row-player/staff-accueuil-section-row-player.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotificationComponent,
-    NavbarComponent,
-    FooterComponent,
+    declarations: [
+        AppComponent,
+        NotificationComponent,
+        NavbarComponent,
+        FooterComponent,
 
-    AccueilComponent,
-    WelcomeAccueilSectionComponent,
-    AboutDetailsAccueilSectionComponent,
-    NewsAccueilSectionComponent,
-    SocialsAccueilSectionComponent,
-    StaffAccueilSectionComponent,
-    AboutAccueilSectionComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgbModule,
-    NewsModule,
-    NgOptimizedImage
-  ],
-  providers: [
-    Title,
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    provideHttpClient(withFetch())
-  ],
-  exports: [
-    NotificationComponent
-  ],
-  bootstrap: [AppComponent]
+        AccueilComponent,
+        WelcomeAccueilSectionComponent,
+        AboutDetailsAccueilSectionComponent,
+        NewsAccueilSectionComponent,
+        SocialsAccueilSectionComponent,
+        StaffAccueilSectionComponent,
+        StaffAccueuilSectionRowPlayerComponent,
+        AboutAccueilSectionComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        NgbModule,
+        NewsModule,
+        NgOptimizedImage,
+        MinecraftHeadComponent
+    ],
+    providers: [
+        Title,
+        provideClientHydration(),
+        provideAnimationsAsync(),
+        provideHttpClient(withFetch())
+    ],
+    exports: [
+        NotificationComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
