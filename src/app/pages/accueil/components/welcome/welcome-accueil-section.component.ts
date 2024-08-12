ca1ba48a85c5c4f63511daf96e626c4a38bcc205
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2024-08-12 09:42:46 +0200

    add player count on welcome page (#70)

--- src/app/pages/accueil/components/welcome/welcome-accueil-section.component.ts
+++ src/app/pages/accueil/components/welcome/welcome-accueil-section.component.ts
@@ -1,12 +1,26 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {PacifistaServerInfoService} from "@funixproductions/funixproductions-requests";
+import {environment} from "../../../../../environments/environment";
 
 @Component({
   selector: 'welcome-section',
   templateUrl: './welcome-accueil-section.component.html',
   styleUrls: ['./welcome-accueil-section.component.scss']
 })
-export class WelcomeAccueilSectionComponent {
+export class WelcomeAccueilSectionComponent implements AfterViewInit {
 
   playersAmount: number = 0;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngAfterViewInit(): void {
+    new PacifistaServerInfoService(this.httpClient, environment.production).getStatus().subscribe({
+      next: value => {
+        this.playersAmount = value.onlinePlayers
+      }
+    })
+  }
 
 }
